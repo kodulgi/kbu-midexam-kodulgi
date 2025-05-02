0c8b5dedf965e2145bf1b9daa0460d4acf9a33d1
kodulgi <cjylove0107@gmail.com>
2025-05-02 09:43:22 +0900

    [Q1] fix: 모음 개수 세기 및 sum.sh 파일 추가

--- scripts/count_vowels.sh
+++ scripts/count_vowels.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
 
-count=$(grep -o '[aeiou]' sample.txt | wc -w)
-echo "Total vowels: $count"
+#!/bin/bash
+
+# sample.txt 파일에서 모음을 찾고 개수를 셈
+count=$(grep -o -i '[aeiou]' scripts/sample.txt | wc -l)
+
+# 모음 개수를 출력
+echo "모음의 개수: $count"
